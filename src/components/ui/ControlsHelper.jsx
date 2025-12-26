593f341fdacfc3fddac594e9cf105667ff75b4d9
ErickDKnight <erickmontesdk@gmail.com>
2025-12-26 12:49:53 -0800

    forcing light mode and adjusting help text to device

--- src/components/ui/ControlsHelper.jsx
+++ src/components/ui/ControlsHelper.jsx
@@ -2,21 +2,28 @@ import React from 'react';
 import '../../styles/helpers.css';
 
 const ControlsHelper = ({ isHelperOn }) => {
+    const [isTouch, setIsTouch] = React.useState(false);
+
+    React.useEffect(() => {
+        // Simple check for touch device
+        setIsTouch(window.matchMedia("(pointer: coarse)").matches);
+    }, []);
+
     if (!isHelperOn) return null;
 
     return (
         <div className="controls-helper-container">
             <div className="helper-tag comic-box">
-                <i className="fa-solid fa-mouse-left-click"></i>
-                <span>Left Click: Rotate</span>
+                <i className={`fa-solid ${isTouch ? 'fa-hand-pointer' : 'fa-mouse-left-click'}`}></i>
+                <span>{isTouch ? '1 Finger: Rotate' : 'Left Click: Rotate'}</span>
             </div>
             <div className="helper-tag comic-box">
                 <i className="fa-solid fa-arrows-up-down"></i>
-                <span>Scroll: Zoom</span>
+                <span>{isTouch ? 'Pinch: Zoom' : 'Scroll: Zoom'}</span>
             </div>
             <div className="helper-tag comic-box">
-                <i className="fa-solid fa-mouse-right-click"></i>
-                <span>Right Click: Pan</span>
+                <i className={`fa-solid ${isTouch ? 'fa-hand-peace' : 'fa-mouse-right-click'}`}></i>
+                <span>{isTouch ? '2 Fingers: Pan' : 'Right Click: Pan'}</span>
             </div>
             <div className="helper-tag comic-box interactive">
                 <i className="fa-solid fa-lightbulb"></i>
